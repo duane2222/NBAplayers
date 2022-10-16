7985b8c21013b25e268adb6fa7423a01887f7242
Jared Moon <115386381+duane2222@users.noreply.github.com>
2022-10-15 23:49:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,41 +11,40 @@ document.getElementById("Submit").addEventListener("click", function(event) {
     return response.json();
   }).then(function(json) {
     let results = "";
-    console.log(json)
-    console.log(json.data[0].id)
+    
+  
     playerID = json.data[0].id
+     results += "<div class='main'>";
     results += '<h1 id="Header">You chose ' + json.data[0].first_name + " " +json.data[0].last_name + "</h1>";
     results += "<p id='description'>";
     results += "<h3>" + json.data[0].first_name + " " +json.data[0].last_name + " is " + json.data[0].height_feet + "ft " + json.data[0].height_inches + " inches tall, Wow!</h3>";
     results += "<h3>He played for the " + json.data[0].team.full_name + " in the " + json.data[0].team.division + " Divison which is apart of the " + json.data[0].team.conference + "ern Conference</h3>";
     results += '</p>';
+    results += "</div>";
   
     const url2 = "https://www.balldontlie.io/api/v1/season_averages?player_ids[]=" + playerID;
    fetch(url2)
     .then(function(response) {
         return response.json();
       }).then(function(extra) {
-          console.log(extra.data[0].games_played)
-          results += "He played for " + extra.data[0].games_played + "games."
+          results += "<hr color='black'>";
+          results += "<div class='main'>";
+          results += "<h1>" + json.data[0].first_name + " " + json.data[0].last_name  + "'s 2021 season</h1>";
+          results += "<h3>He played for " + extra.data[0].games_played + " games with an average playtime of " + extra.data[0].min + ". </h3>";
+          results += "<h3>During those games, he averaged " + extra.data[0].pts + " points with an average of " + extra.data[0].ast + " assists</h3>";
+          results += "<h3>He also had " + extra.data[0].reb + " rebounds on average and " + extra.data[0].stl + " steals</h3>";
+          results += "</div>";
+          document.getElementById("Results").innerHTML = results;   
       })
   .catch(error => {
-    document.getElementById("Results").innerHTML = "";
-    document.getElementById('forecast').style.border= "0px";
+    let results = "<h2>Error</h2>";
+    results += "<p id='errorMessage'>Player couldn't be found. Please check your spelling and try again</p>";
+    document.getElementById("Results").innerHTML = results;
   });
-  
-  
-  
-  
-    document.getElementById("Results").innerHTML = results;   
-    
-    
-    
-
-    
   })
   .catch(error => {
     let results = "<h2>Error</h2>";
-    results += "<p id='errorMessage'>City couldn't be found. Please check your spelling and try again</p>";
+    results += "<p id='errorMessage'>Player couldn't be found. Please check your spelling and try again</p>";
     document.getElementById("Results").innerHTML = results;
   });
   
